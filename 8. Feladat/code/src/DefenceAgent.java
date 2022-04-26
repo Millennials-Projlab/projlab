d0b2b934d43c89056f3db8027f7a62cf19cad776
Heniboj <berenyihenrik2001@gmail.com>
2022-04-26 22:12:53 +0200

    DefenceAgent

--- 8. Feladat/code/src/DefenceAgent.java
+++ 8. Feladat/code/src/DefenceAgent.java
@@ -2,28 +2,30 @@
 * Defence Agens osztály
 */
 public class DefenceAgent extends Agent {
-	
 	public DefenceAgent(Genetics g, int l) {
 		super(g, l);
-		// TODO Auto-generated constructor stub
 	}
-
 
 	/** 
 	 * Elindítja az ágens effektjét a paraméterként megadott virológuson
 	 * @param virologist
 	 */
-	public void Effect(Virologist virologist) {
-		System.out.println("Defence Agent has its effect on  "+virologist.getName()+".");
+	public void startEffect(Virologist virologist) {
+		effect = true;
+		virologist.setEffectFlag(3);
 	}
 
+	public void endEffect(Virologist virologist) {
+		virologist.setEffectFlag(0);
+	}
 	
 	/** 
 	 * Megfertőzi a paraméterként megadott virológust.
 	 * @param target
 	 */
 	public void infect(Virologist target) {
-		target.setEffectFlag(1);
+		target.addEffect(this);
+		startEffect(target);
 	}
 
 	public boolean isSame(String agentName) {
